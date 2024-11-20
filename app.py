95fab316b81893c0d0dd6dcb054f62a9e3788096
Clement <clem@gitea.dev>
2024-11-20 11:56:12 +0100

    added default calculator functions

---
+++ app.py
@@ -0,0 +1,21 @@
+def multiplication(x, y):
+    return x * y
+
+def division(x, y):
+    return x / y
+
+def addition(x, y):
+    return x + y
+
+def soustraction(x, y):
+    return x - y
+
+def puissance(x, y):
+    return x ** y
+
+if __name__ == '__main__':
+    print(multiplication(2, 3))
+    print(division(6, 3))
+    print(addition(2, 3))
+    print(soustraction(3, 2))
+    print(puissance(2, 3))
